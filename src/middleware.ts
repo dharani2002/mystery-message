150ec20024a1372309cfb0454a0fd6a93a77d51c
dharani2002 <rdharani25@gmail.com>
2025-01-20 14:15:53 +0530

    sign-up page using shadcn

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,7 +26,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         
-        '/sign-up',
         '/',
         '/dashboard/:path*',
         '/verify/:path'
--- src/schema/signUp.schema.ts
+++ src/schema/signUp.schema.ts
@@ -6,7 +6,7 @@ export const usernameValidation=z
     .max(20,"Username must be no more than 20 characters")
     .regex(/^[a-zA-Z0-9_]+$/,"username must not contain speacial character")
 
-export const singupSchema=z.object({
+export const signupSchema=z.object({
     username:usernameValidation,
     email:z.string().email({message:"inavalid email address"}),
     password:z.string().min(6,{message:"password must be atleast 6 characters"})
